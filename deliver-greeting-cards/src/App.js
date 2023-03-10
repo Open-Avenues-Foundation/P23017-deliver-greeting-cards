0cbf0295bd0f76c056a8a2ac5b6fb8f12fb1091d
karankwatra <karankwatra@Karans-MacBook-Pro.local>
2023-03-10 00:10:48 -0500

    react-router

--- deliver-greeting-cards/src/App.js
+++ deliver-greeting-cards/src/App.js
@@ -1,22 +1,52 @@
-import './App.css';
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/create-user">Create a User</Link>
+                </li>
+                <li>
+                  <Link to="/view-users">View Users</Link>
+                </li>
+                <li>
+                  <Link to="/create-event">Create an Event</Link>
+                </li>
+                <li>
+                  <Link to="/view-events">View Events</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Outlet />
+          </>
+        }>
+          <Route path="" element={
+            <h1>Home</h1>
+          } />
+          <Route path="create-user" element={
+            <h1>Create a User</h1>
+          } />
+          <Route path="view-users" element={
+            <h1>Contact Me</h1>
+          } />
+          <Route path="create-event" element={
+            <h1>Create an Event</h1>
+          } />
+          <Route path="view-events" element={
+            <h1>View Events</h1>
+          } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

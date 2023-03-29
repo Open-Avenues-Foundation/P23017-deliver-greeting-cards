b967b38942a64d5919e9c55f0b77199ac4ec4ed2
karankwatra <karankwatra@Karans-MacBook-Pro.local>
2023-03-29 16:56:26 -0700

    require cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pg = require('pg');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json()) // for parsing application/json

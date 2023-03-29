3b23c07cd300025e88dfc2f8a24d9fa1f6f5eadf
karankwatra <karankwatra@Karans-MacBook-Pro.local>
2023-03-29 16:02:02 -0700

    cosmetic updates

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const pg = require('pg');
 
 const app = express();
 app.use(bodyParser.json()) // for parsing application/json
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
@@ -16,85 +17,69 @@ const client = new pg.Client({
 });
 client.connect();
 
-app.use(express.json());
-
-
 app.get('/api', (req, res) => {
   res.send('Hello World!');
 });
 
-// Sayeed
+// this endpoint should return all events in the database
 app.get('/api/events', async (req, res) => {
   const response = await client.query('SELECT * FROM events');
   res.send(response.rows);
-  // TODO
-  // this endpoint should return all events in the database
 });
 
-// Gabby
+// this endpoint should return an event by its id
 app.get('/api/events/:id', async (req, res) => {
-  // TODO
-  // this endpoint should return an event by its id
   const response = await client.query('SELECT * FROM events WHERE id = $1', [req.params.id]);
   res.send(response.rows);
 });
 
-// Damaris
+// this endpoint should create a new event in the database
 app.post('/api/events', async (req, res) => {
-  // TODO
   const event_type = req.body.event_type;
   const date = req.body.date;
   const user_id = req.body.user_id;
-  const response = await client.query('INSERT INTO events (event_type, date, user_id) VALUES($1, $2, $3) RETURNING *',[
-  event_type,
-  date,
-  user_id,
+  const response = await client.query('INSERT INTO events (event_type, date, user_id) VALUES($1, $2, $3) RETURNING *', [
+    event_type,
+    date,
+    user_id,
   ]);
   res.send(response.rows[0]);
-  // this endpoint should create a new event in the database
 });
 
-// Tameem
+// this endpoint should update an event by its id
 app.patch('/api/events/:id', async (req, res) => {
-  // TODO
-  var response = 0; 
-  if(req.body.event_type != null){
-     response = await client.query('UPDATE events SET event_type = $1 WHERE id = $2 RETURNING *', [req.body.event_type ,req.params.id]);
-  }if(req.body.date != null){
-     response = await client.query('UPDATE events SET date = $1 WHERE id = $2 RETURNING *', [req.body.date ,req.params.id]);
-  } if(req.body.user_id != null){
-     response = await client.query('UPDATE events SET user_id = $1 WHERE id = $2 RETURNING *', [req.body.user_id ,req.params.id]);
+  var response = 0;
+  if (req.body.event_type != null) {
+    response = await client.query('UPDATE events SET event_type = $1 WHERE id = $2 RETURNING *', [req.body.event_type, req.params.id]);
+  } if (req.body.date != null) {
+    response = await client.query('UPDATE events SET date = $1 WHERE id = $2 RETURNING *', [req.body.date, req.params.id]);
+  } if (req.body.user_id != null) {
+    response = await client.query('UPDATE events SET user_id = $1 WHERE id = $2 RETURNING *', [req.body.user_id, req.params.id]);
   }
   res.send(response.rows);
-  // this endpoint should update an event by its id
 });
 
-// Tameem
+// this endpoint should return all users in the database
 app.get('/api/users', async (req, res) => {
-  // TODO
   const response = await client.query('SELECT * FROM users');
   res.send(response.rows);
-  // this endpoint should return all users in the database
 });
 
-// Damaris
+// this endpoint should return a user by their id 
 app.get('/api/users/:id', async (req, res) => {
-  // TODO
   const id = req.params.id;
   const response = await client.query('SELECT * FROM users WHERE id=$1', [id]);
   const userRow = response.rows[0];
-  if(!userRow){
+  if (!userRow) {
     res.status(404).send('HTTP Not Found Error');
     return;
   }
   res.send(userRow);
-  // this endpoint should return a user by their id 
 });
 
-// Sayeed 
+// this endpoint should create a new user in the database 
+// currently capable of taking static values in Postman and entering directly into DB
 app.post('/api/users', async (req, res) => {
-  // currently capable of taking static values in Postman and entering directly into DB
-  // TODO: adjust to be dynamic
   const name = req.body.name;
   const address_id = req.body.address_id;
   const data_row = await client.query('INSERT INTO users (name, address_id) VALUES ($1, $2) Returning *', [
@@ -102,17 +87,15 @@ app.post('/api/users', async (req, res) => {
     address_id,
   ]);
   res.send(data_row.rows[0]);
-  // this endpoint should create a new user in the database 
 });
 
-// Gabby
+// this endpoint should update a user by its id
 app.patch('/api/users/:id', async (req, res) => {
-  // TODO
   var response = 0;
-  if(req.body.name != null){
-    response = await client.query('UPDATE users SET name = $1 WHERE id = $2 RETURNING *', [req.body.name,req.params.id]);
+  if (req.body.name != null) {
+    response = await client.query('UPDATE users SET name = $1 WHERE id = $2 RETURNING *', [req.body.name, req.params.id]);
   }
-  if(req.body.address_id != null){
+  if (req.body.address_id != null) {
     response = await client.query('UPDATE users SET address_id = $1 WHERE id = $2 RETURNING *', [req.body.address_id, req.params.id]);
   }
   res.send(response.rows);
